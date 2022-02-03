ab45acf88c2799fe97e3a82f34876c5fc670a58a
Rebecca Jackson <rbca.jackson@gmail.com>
2022-02-03 13:02:36 -0800

    Update Makefile, update imports, add new experimental evidence axioms

--- src/ontology/imports/get_terms.py
+++ src/ontology/imports/get_terms.py
@@ -1,24 +1,28 @@
 import re
 import sys
 
-eco_path = "src/ontology/modules/obi_logic.owl"
-ns = sys.argv[1]
-term_file_path = "src/ontology/imports/" + ns + "_terms.txt"
-
+eco_path = sys.argv[1]
+ns = sys.argv[2]
+output = sys.argv[3]
 regex = re.compile(ns + "_[0-9]{7}")
+
 
 def file_to_str(filename):
 	with open(filename) as f:
 		return f.read().lower()
 
+
 def get_obi_terms(s):
 	return (term for term in re.findall(regex, s))
 
+
 if __name__ == '__main__':
-    curies = []
+    curies = set()
+    print(eco_path)
     for term in get_obi_terms(file_to_str(eco_path)):
         curie = term.replace("_", ":").upper()
-        curies.append(curie)
-    term_file = open(term_file_path, "w")
+        curies.add(curie)
+    curies = sorted(curies)
+    term_file = open(output, "w")
     term_file.write("\n".join(curies))
     term_file.close()
